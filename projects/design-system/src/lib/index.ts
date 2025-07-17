d3c18cf9141ccb5eeb791f4bc086ad088faf1215
kathryngraysonnanz <kathrynkaygrayson@gmail.com>
2025-07-17 16:43:07 -0400

    working on build export

---
+++ projects/design-system/src/lib/index.ts
@@ -0,0 +1,5 @@
+/* This file is what's referenced when we build our custom component library for dist – make sure you add new components in here! */ 
+
+export * from '../../../../src/stories/button.component'; 
+export * from '../../../../src/stories/card.component';
+export * from '../../../../src/stories/datetimepicker.component'; 
